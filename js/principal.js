82042bfddb2156f13f6bbcb3e2fe70c5f5695339
Daniel Carlos <84096317+danielcarlos-s@users.noreply.github.com>
2021-08-23 12:06:09 -0300

    Validação de dados
    calculando IMC
    validando dados de altura e peso

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,31 @@
-var titulo = document.querySelector(".titulo");
-console.log(titulo.textContent);
+/* var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista"; */
 
-titulo.textContent = "Aparecida Nutricionista";
+var paciente = document.querySelector("#primeiro-paciente"); // paciente recebe todos os dados do #primeiro-paciente
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdImc = paciente.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso Inválido!");
+    pesoValido = false;
+    tdPeso.textContent = "Peso inválido!";
+}
+
+if(altura <= 0 || altura >= 3.00){
+    alturaValida = false;
+    console.log("Altura Inválida!");
+    tdAltura.textContent = "Altura inválida!";
+}
+
+if(pesoValido && alturaValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
