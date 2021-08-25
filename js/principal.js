066e0d29105351e7e1c6ea45465fd5886b327780
Daniel Carlos <84096317+danielcarlos-s@users.noreply.github.com>
2021-08-25 11:59:51 -0300

    Update 1.0
    função de adicionar novo paciente feita

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,5 @@
-/* var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista"; */
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente"); // pacientes recebe todos os dados do #primeiro-pacientes
 
@@ -23,7 +23,7 @@ for (var i = 0; i < pacientes.length; i++) {
         tdPeso.textContent = "Peso inválido!";
         paciente.classList.add('paciente-invalido'); // adiciona uma classe no css.
     }
-    
+
     if (altura <= 0 || altura >= 3.00) {
         alturaValida = false;
         console.log("Altura Inválida!");
@@ -36,3 +36,45 @@ for (var i = 0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var botaoAdicionar = document.querySelector('#adicionar-paciente');
+
+botaoAdicionar.addEventListener('click', function (evento) {
+    event.preventDefault(); // evita que o botão atualize a página
+
+    var form = document.querySelector(".form-adiciona"); //buscando o form
+
+    // variável recebendo os valores preenchidos no formulário
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    // criando um elemento "tr" na variável
+    var pacienteTr = document.createElement("tr");
+
+    // criando um elemento "td" na variável
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    // atribuindo o conteúdo de texto preenchido, as respectivas td's
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    // colocando como filhos o td ao pacienteTr (tag tr)
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    // buscando a tabela e definindo o pacienteTr como filho dela
+    var tabela = document.querySelector('#tabela-pacientes');
+
+    tabela.appendChild(pacienteTr);
+
+})
