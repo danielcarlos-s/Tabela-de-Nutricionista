a31b06c5d35ab02d55124175c6f00e4f84217144
Daniel Carlos <84096317+danielcarlos-s@users.noreply.github.com>
2021-08-26 11:47:02 -0300

    Update 1.2
    Redução do código com boas práticas de programação.

--- js/form.js
+++ js/form.js
@@ -1,43 +1,54 @@
 var botaoAdicionar = document.querySelector('#adicionar-paciente');
 
-botaoAdicionar.addEventListener('click', function (evento) {
+botaoAdicionar.addEventListener('click', function (event) {
     event.preventDefault(); // evita que o botão atualize a página
 
-    var form = document.querySelector(".form-adiciona"); //buscando o form
+    var form = document.querySelector(".form-adiciona"); //buscando o formulário
+    var paciente = obtemPacienteDoFormulario(form); // Extraindo informações do paciente do formulário
+    var pacienteTr = montaTr(paciente); // Montando a tr com os dados do paciente
 
-    // variável recebendo os valores preenchidos no formulário
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    // criando um elemento "tr" na variável
-    var pacienteTr = document.createElement("tr");
-
-    // criando um elemento "td" na variável
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    // atribuindo o conteúdo de texto preenchido, as respectivas td's
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso,altura); //usando a função do arquivo calcula-imc.js
-
-    // colocando como filhos o td ao pacienteTr (tag tr)
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
-
-    // buscando a tabela e definindo o pacienteTr como filho dela
-    var tabela = document.querySelector('#tabela-pacientes');
-
+    var tabela = document.querySelector('#tabela-pacientes'); // buscando a tabela e definindo o pacienteTr como filho dela
     tabela.appendChild(pacienteTr);
 
+    form.reset(); // reseta o formulário após enviar os dados
+
 })
+
+// função que obtém os dados do paciente inseridos no formulário
+function obtemPacienteDoFormulario(form) {
+
+    // a variável paciente é um objeto com várias informações
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+}
+
+// função que monta a tr
+function montaTr(paciente) {
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    // colocando como filhos o td ao pacienteTr (tag tr), com a função de criação de td
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+// função que mota as td's
+function montaTd(dado, classe) {
+    var td = document.createElement("td"); // cria a td
+    td.textContent = dado; // adiciona os dados recebidos e insere na td
+    td.classList.add(classe); // adiciona a classe recebida na td
+
+    return td;
+}
